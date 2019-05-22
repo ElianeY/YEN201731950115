d1f4efdd62e91b2cd9f012fc05105f2aef0aa9c2
ElianeY <2077684606@qq.com>
2019-05-22 11:55:27 +0800

    v2

---
+++ YEN201731950115/Controllers/SecondController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YEN201731950115.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult twice()
+        {
+            ViewBag.a = "欢迎来到我的世界！";
+            return View();
+        }
+    }
+}
